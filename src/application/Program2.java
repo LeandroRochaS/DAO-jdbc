ac7d3d9652ce3d76c1fa48ee1d5fd5af49986f20
Leandro Rocha Santos <111878961+LeandroRochaS@users.noreply.github.com>
2023-05-08 15:34:22 -0300

    Implemeted Delete

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,20 +1,13 @@
 package application;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
-import model.dao.SellerDao;
 import model.entities.Department;
-import model.entities.Seller;
+
 
 public class Program2 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("=== TESTE 1: department findById ===");
@@ -25,10 +18,13 @@ public class Program2 {
 		System.out.println(departmentDao.findAll());
 		
 		System.out.println("=== TESTE 3: department Insert ===");
-		//departmentDao.insert(dep);
+		departmentDao.insert(dep);
 		
 		System.out.println("=== TESTE 4: department Update ===");
-		//departmentDao.update(dep);
+		departmentDao.update(dep);
+		
+		System.out.println("=== TESTE 5: department Delete ===");
+		departmentDao.deleteById(7);
 
 		
 		
